d67f399efc5c02cdf8e1db372d21b8c11e6a9245
Stacy Dunegan <skdunegan@gmail.com>
2024-09-08 21:17:10 -0400

    moving MCC to its own repo

--- mcc/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="fixed bottom-0 mx-auto w-full">
-      <p className="text-center">
-        <a href="https://github.com/DuneganS/things/mcc">GitHub</a>
-      </p>
-    </footer>
-  );
-}
--- mcc/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <p className="underline">Welcome to the home page</p>
-    </main>
-  );
-}
